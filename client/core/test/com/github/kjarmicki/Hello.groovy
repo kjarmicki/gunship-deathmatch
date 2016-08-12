a28f363379d97a894e41586517b70e169754e8d8
kjarmicki <jarmicki@gmail.com>
2016-08-12 20:27:48 +0200

    spock for testing

---
+++ client/core/test/com/github/kjarmicki/Hello.groovy
@@ -0,0 +1,13 @@
+package com.github.kjarmicki
+
+import spock.lang.Specification
+
+class Hello extends Specification {
+    def 'hello test'() {
+        when:
+        true
+
+        then:
+        true
+    }
+}
