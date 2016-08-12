6891efc8904acae524777889e99549839903003d
kjarmicki <jarmicki@gmail.com>
2016-08-12 20:07:56 +0200

    structure

---
+++ client/android/src/com/github/kjarmicki/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.kjarmicki;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.kjarmicki.GunshipDeathmatch;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GunshipDeathmatch(), config);
+	}
+}
---
+++ client/core/src/com/github/kjarmicki/GunshipDeathmatch.java
@@ -0,0 +1,18 @@
+package com.github.kjarmicki;
+
+import com.badlogic.gdx.Game;
+
+public class GunshipDeathmatch extends Game {
+
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
