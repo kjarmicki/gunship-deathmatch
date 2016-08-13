0434416161b482a5ea01edd11455d06cf9262ec3
kjarmicki <jarmicki@gmail.com>
2016-08-13 13:13:51 +0200

    use polygon as player shape

--- client/core/src/com/github/kjarmicki/model/ShipModel.java
+++ client/core/src/com/github/kjarmicki/model/ShipModel.java
@@ -1,5 +1,6 @@
 package com.github.kjarmicki.model;
 
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
@@ -7,37 +8,45 @@ public class ShipModel {
     public static final int WIDTH = 236;
     public static final int HEIGHT = 233;
     public static final Vector2 MOVE_SPEED = new Vector2(10, 10);
-    private final Rectangle takenArea;
+    private final Polygon takenArea;
 
     public ShipModel(float x, float y) {
-        takenArea = new Rectangle();
-        takenArea.x = x;
-        takenArea.y = y;
-        takenArea.width = WIDTH;
-        takenArea.height = HEIGHT;
+        takenArea = new Polygon(rectangularVertices(x, y, WIDTH, HEIGHT));
     }
 
     public float getX() {
-        return takenArea.x;
+        return takenArea.getX();
     }
 
     public float getY() {
-        return takenArea.y;
+        return takenArea.getY();
     }
 
     public void moveUp() {
-        takenArea.y += MOVE_SPEED.y;
+        float x = getX();
+        float y = getY() + MOVE_SPEED.y;
+        takenArea.setPosition(x, y);
     }
 
     public void moveDown() {
-        takenArea.y -= MOVE_SPEED.y;
+        float x = getX();
+        float y = getY() - MOVE_SPEED.y;
+        takenArea.setPosition(x, y);
     }
 
     public void moveLeft() {
-        takenArea.x -= MOVE_SPEED.x;
+        float x = getX() - MOVE_SPEED.x;
+        float y = getY();
+        takenArea.setPosition(x, y);
     }
 
     public void moveRight() {
-        takenArea.x += MOVE_SPEED.x;
+        float x = getX() + MOVE_SPEED.x;
+        float y = getY();
+        takenArea.setPosition(x, y);
+    }
+
+    private float[] rectangularVertices(float x, float y, float width, float height) {
+        return new float[] {x, y, width, x, width, height, y, height};
     }
 }
