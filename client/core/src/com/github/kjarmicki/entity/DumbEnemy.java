df3e7eec66174dc2a85a4bb5de661b192d7b0bea
kjarmicki <jarmicki@gmail.com>
2016-10-26 01:03:14 +0200

    enemy is shooting

--- client/core/src/com/github/kjarmicki/entity/DumbEnemy.java
+++ client/core/src/com/github/kjarmicki/entity/DumbEnemy.java
@@ -2,13 +2,18 @@ package com.github.kjarmicki.entity;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.github.kjarmicki.ship.Ship;
 import com.github.kjarmicki.ship.ShipOwner;
+
+import java.sql.Time;
 
 public class DumbEnemy implements ShipOwner {
     public static final float DEFAULT_X = 500;
     public static final float DEFAULT_Y = 500;
+    public static final long SHOOTING_INTERVAL = 1000;
     private Ship ship;
+    private long lastShot = TimeUtils.millis() - SHOOTING_INTERVAL;
 
     public void checkPlacementWithinBounds(Rectangle bounds) {
         ship.checkPlacementWithinBounds(bounds);
@@ -19,11 +24,22 @@ public class DumbEnemy implements ShipOwner {
     }
 
     public void update(float delta) {
+        shoot(delta);
         ship.update(delta);
     }
 
     public void draw(Batch batch) {
         ship.draw(batch);
+    }
+
+    private void shoot(float delta) {
+        if(TimeUtils.millis() - lastShot > SHOOTING_INTERVAL) {
+            ship.startShooting(delta);
+            lastShot = TimeUtils.millis();
+        }
+        else {
+            ship.stopShooting(delta);
+        }
     }
 
     @Override
