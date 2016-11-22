3c62bd44be5dd2197d78a5f867f1df538e4c9158
kjarmicki <jarmicki@gmail.com>
2016-11-22 23:53:16 +0100

    server plan

--- server/src/com/github/kjarmicki/server/GunshipDeathmatchServer.java
+++ server/src/com/github/kjarmicki/server/GunshipDeathmatchServer.java
@@ -7,6 +7,18 @@ public class GunshipDeathmatchServer {
     public static final String host = "localhost";
     public static final int port = 3000;
 
+    /*
+     * TODO base plan for server side logic with one player
+     * 1. server starts up the game
+     *      1a. separate game containers from rendering into game class, make client still working
+     *      2a. lookup headless libgdx
+     * 2. server sets up connections and events
+     * 3. client connects to server
+     * 4. server responds with client spawn point
+     * 5. client moves through the world and sends input updates
+     * 6. ...client side prediction, server reconciliation, entity interpolation
+     */
+
     public static void main(String[] args) {
         Configuration config = new Configuration();
         config.setHostname(host);
